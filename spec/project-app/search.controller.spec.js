eadde0efa674027f8aaa6afc815c7bd60267873c
Mykal <mcnichol.m@gmail.com>
2016-03-08 09:19:32 -0600

    Create project app controller folders

---
+++ spec/project-app/search.controller.spec.js
@@ -0,0 +1,24 @@
+describe('Search Controller',function(){
+    var $scope;
+    var $location;
+
+    beforeEach(module('projectApp'));
+
+    beforeEach(inject(function(_$controller_, _$location_){
+        $scope = {};
+        $location = _$location_;
+        _$controller_('SearchController', {$scope: $scope, $location: $location});
+    }));
+
+    it('should redirect to the query results page for non-empty query', function(){
+        $scope.query = 'project one';
+        $scope.search();
+        expect($location.url()).toBe('/location?q=project%20one');
+    });
+
+    it('should not redirect to query results for empty query', function(){
+        $scope.query = '';
+        $scope.search();
+        expect($location.url()).toBe('');
+    })
+});
---
+++ src/project-app/search.controller.js
@@ -0,0 +1,8 @@
+angular.module('projectApp',[])
+    .controller('SearchController',function($scope,$location){
+        $scope.search = function(){
+            if($scope.query){
+                $location.path('/location').search('q',$scope.query);
+            }
+        }
+    });
